7ce14e8c0b000e8e397ec32025fbabd6b33771f3
dionp <dionpeci08@gmail.com>
2025-03-03 19:29:21 +0100

    Blabla

--- NoiseDemo/Program.cs
+++ NoiseDemo/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5174")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials(); // Allow credentials if you need to send cookies or authentication headers
